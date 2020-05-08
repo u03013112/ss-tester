b5dddfdcc064c8ff66a3536a86bf2cb519dfd9c5
u03013112@hotmail.com <u03013112@hotmail.com>
2020-05-08 19:10:19 +0800

    bug

--- mod/mod.go
+++ mod/mod.go
@@ -23,7 +23,7 @@ func AddTestSSConfig(configList []TestSSConfig) {
 		if db.Error != nil {
 			sql.GetInstance().Create(&config)
 		} else {
-			sql.GetInstance().Update(&config)
+			sql.GetInstance().Model(&config).Updates(&config)
 		}
 	}
 }
